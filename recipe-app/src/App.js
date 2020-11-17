38c1651d13d06c217fbdd05ff177e2adae9dbbc5
khaledrefaat <khaledrefaat08@gmail.com>
2020-11-17 08:57:01 +0200

    recipe-app first commit

---
+++ recipe-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Search from './components/Search';
+const App = () => {
+	const [ App_Id, App_Api ] = [ '137d9ef1', '159f9b7d8e7bec9a64074654382c858e' ];
+
+	return (
+		<div>
+			<Search />
+		</div>
+	);
+};
+
+export default App;
