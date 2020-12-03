b46523863939b041ce4932f7bf671ca6be96ecda
khaledrefaat <khaledrefaat08@gmail.com>
2020-12-03 04:30:19 +0200

    bulletin-board is working ^_^

--- bulletin-board/src/components/App.js
+++ bulletin-board/src/components/App.js
@@ -8,9 +8,46 @@ const App = () => {
 	const [ item, setItem ] = useState('');
 	const [ items, setItems ] = useState(JSON.parse(localStorage.getItem('items')) || []);
 
-	const keyPress = event => {};
+	useEffect(
+		() => {
+			localStorage.setItem('items', JSON.stringify(items));
+		},
+		[ items ]
+	);
 
-	const newItem = () => {};
+	const keyDown = event => {
+		const code = event.keyCode || event.which;
+		if (code === 13) {
+			newItem();
+		}
+	};
+
+	const newItem = () => {
+		if (item.trim() !== '') {
+			// if item is not blank, create a new item object
+			const newItem = {
+				id: uuidv4(),
+				item: item,
+				color: randomColor({ luminosity: 'light' }),
+				defaultPros: { x: 100, y: 0 }
+			};
+			setItems(items => [ ...items, newItem ]);
+			setItem('');
+		} else {
+			alert('Enter a new item');
+			setItem('');
+		}
+	};
+
+	const updatePos = (data, index) => {
+		let newArr = [ ...items ];
+		newArr[index].defaultPos = { x: data.x, y: data.y };
+		setItems(newArr);
+	};
+
+	const deleteNote = id => {
+		setItems(items.filter(item => item.id !== id));
+	};
 
 	return (
 		<div className="container">
@@ -19,15 +56,27 @@ const App = () => {
 				value={item}
 				onChange={e => setItem(e.target.value)}
 				placeholder="Enter Something..."
-				onKeyPress={e => keyPress(e)}
+				onKeyDown={e => keyDown(e)}
 			/>
 			<button className="btn" onClick={newItem}>
 				Enter
 			</button>
-			<p className="item">
-				{' '}
-				this is nothing and we pretend that this is nothing and everybody pretends that this is nothing
-			</p>
+			{items.map((item, index) => {
+				return (
+					<Draggable
+						key={item.id}
+						defaultPosition={item.defaultPos}
+						onStop={(e, data) => {
+							updatePos(data, index);
+						}}
+					>
+						<div style={{ backgroundColor: item.color }} className="box item">
+							{`${item.item}`}
+							<i className="delete fas fa-times" id="delete" onClick={e => deleteNote(item.id)} />
+						</div>
+					</Draggable>
+				);
+			})}
 		</div>
 	);
 };
