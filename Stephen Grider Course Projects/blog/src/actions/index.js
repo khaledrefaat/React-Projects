e270ea9e9b30d79a793e2aca7709232d4eb637f1
khaledrefaat <khaledrefaat08@gmail.com>
2020-12-05 10:52:36 +0200

    making fetch posts and users better

--- Stephen Grider Course Projects/blog/src/actions/index.js
+++ Stephen Grider Course Projects/blog/src/actions/index.js
@@ -1,15 +1,20 @@
 import _ from 'lodash';
 import jsonPlaceHolder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+	// loop over the posts and get just unique ids
+	const userIds = _.uniq(_.map(getState().posts, 'userId'));
+
+	userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
 	const response = await jsonPlaceHolder.get('/posts');
 	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// this solution to make the action creator fetch each user just 1 time
-// and this have a side effect that if you want to fetch the user again it wont work so you need to make new action for this
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
 	const response = await jsonPlaceHolder.get(`/users/${id}`);
 	dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
