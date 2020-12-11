c8741c759b63a7779bcbb6c6ecbe54f3c9262cfe
khaledrefaat <khaledrefaat08@gmail.com>
2020-12-11 09:01:00 +0200

    an example of react-router(browserRouter)

--- Stephen Grider Course Projects/streams/client/src/components/App.js
+++ Stephen Grider Course Projects/streams/client/src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const pageOne = () => {
-	return <h1>PageOne</h1>;
+	return (
+		<div>
+			<h1>PageOne</h1>
+			<br />
+			{/* this is bad don't ever do this in react */}
+			{/* <a href="/pagetwo">Navigate To Page Two</a> */}
+			<Link to="/pagetwo">Navigate To Page Two </Link>
+		</div>
+	);
 };
 const pageTwo = () => {
-	return <h1>PageTwo</h1>;
+	return (
+		<div>
+			<h1>PageTwo</h1>
+			<br />
+			{/* this is bad don't ever do this in react */}
+			{/* <a href="/">Navigate To Page One</a> */}
+			<Link to="/">Navigate To Page One </Link>
+		</div>
+	);
 };
 
 const App = () => {
