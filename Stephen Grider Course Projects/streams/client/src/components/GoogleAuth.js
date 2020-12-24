4c901a3d23baca417442f200c036cc787012c6ac
khaledrefaat <khaledrefaat08@gmail.com>
2020-12-24 11:59:53 +0200

    adding button to switch from signin and signout in streams project

--- Stephen Grider Course Projects/streams/client/src/components/GoogleAuth.js
+++ Stephen Grider Course Projects/streams/client/src/components/GoogleAuth.js
@@ -22,10 +22,31 @@ class GoogleAuth extends Component {
 		this.setState({ isSignedIn: this.auth.isSignedIn.get() });
 	};
 
+	onSignIn = () => {
+		this.auth.signIn();
+	};
+
+	onSignOut = () => {
+		this.auth.signOut();
+	};
+
 	renderAuthButton() {
-		if (this.state.isSignedIn === null) return <div>Idk if we are signed In</div>;
-		else if (this.state.isSignedIn) return <div>You Are Signed in</div>;
-		else if (!this.state.isSignedIn) return <div>You Are not Signed In</div>;
+		if (this.state.isSignedIn === null) return null;
+		else if (this.state.isSignedIn) {
+			return (
+				<button onClick={this.onSignOut} className="ui red google button">
+					<i className="google icon" />
+					Sign Out
+				</button>
+			);
+		} else if (!this.state.isSignedIn) {
+			return (
+				<button onClick={this.onSignIn} className="ui blue google button">
+					<i className="google icon" />
+					Sign In
+				</button>
+			);
+		}
 	}
 
 	render() {
