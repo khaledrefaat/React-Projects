e9d6aba37ce09755c0f6268d7ebd6c206a280b91
khaledrefaat <khaledrefaat08@gmail.com>
2021-01-07 23:09:25 +0200

    deleting streams in streams-project

--- Stephen Grider Course Projects/streams/client/src/components/streams/StreamDelete.js
+++ Stephen Grider Course Projects/streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from "react";
+import Modal from "../Modal";
+import history from "../../history";
+import { connect } from "react-redux";
+import { deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-	return <h1>StreamDelete</h1>;
-};
+class StreamDelete extends Component {
+    deleteStream = () => {
+        this.props.deleteStream(this.props.match.params.id);
+        this.onDismiss();
+    };
 
-export default StreamDelete;
+    onDismiss() {
+        history.push("/");
+    }
+
+    actions = (
+        // a shortcut for React.Fragment
+        <>
+            <button className="ui negative button" onClick={this.deleteStream}>
+                Delete
+            </button>
+            <button className="ui button" onClick={this.onDismiss}>
+                Cancel
+            </button>
+        </>
+    );
+    render() {
+        return (
+            <div>
+                <Modal
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this?"
+                    actions={this.actions}
+                    navigate="/"
+                    onDismiss={this.onDismiss}
+                />
+            </div>
+        );
+    }
+}
+
+export default connect(null, { deleteStream })(StreamDelete);
