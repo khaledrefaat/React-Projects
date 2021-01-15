ba596837a2eaa1337cf112bb316353a49f6175bf
khaledrefaat <khaledrefaat08@gmail.com>
2021-01-15 04:36:34 +0200

    reactor audio showing time

--- music-player/src/components/Progress.js
+++ music-player/src/components/Progress.js
@@ -3,20 +3,15 @@ import './Progress.css';
 
 const Progress = ({ progress, duration, currentTime }) => {
     const timeCalc = time => {
-        const timeFloor = Math.floor(time);
-        if (timeFloor < 60 && timeFloor < 10) {
-            return `0:0${timeFloor}`;
-        } else if (timeFloor > 10 && timeFloor <= 60) {
-            return `0:${timeFloor}`;
-        } else if (timeFloor > 60 && timeFloor % 60 < 10) {
-            return `${Math.floor(timeFloor / 60)}:0${timeFloor % 60}`;
-        } else if (timeFloor > 60 && timeFloor % 60 >= 10) {
-            return `${Math.floor(timeFloor / 60)}:${timeFloor % 60}`;
-        }
+        const minutes = Math.floor(time / 60);
+        let seconds = Math.floor(time % 60);
+        if (seconds < 10) seconds = `0${seconds}`;
+        if (seconds) return `${minutes}:${seconds}`;
+        return '0:00';
     };
 
     return (
-        <div className="progress-container">
+        <div className="progress-container" onClick={e => console.log(e)}>
             <div className="progress" style={{ width: `${progress}%` }}></div>
             <div className="duration-wrapper">
                 <span className="current-time">{timeCalc(currentTime)}</span>
