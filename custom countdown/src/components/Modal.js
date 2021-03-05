4297975ac6e08ab40524843266f17a672c086f99
khaledrefaat <khaledrefaat08@gmail.com>
2021-03-05 23:04:47 +0200

    code refactor

--- custom countdown/src/components/Modal.js
+++ custom countdown/src/components/Modal.js
@@ -11,58 +11,39 @@ const Modal = () => {
   const [minutesLeft, setMinutes] = useState(0);
   const [secondsLeft, setSeconds] = useState(0);
 
-  // const today = new Date();
-  // const dd = today.getDate();
-  // const mm = today.getMonth() + 1;
-  // const yyy = today.getFullYear();
-
-  const today = new Date().getTime();
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
   function onFormSubmit() {
     if (dateTerm && term) {
       setShowCountdown(!showCountdown);
-      const countDown = new Date(dateTerm).getTime();
-      const distance = Math.floor(countDown - today);
-      const days = Math.floor((countDown - today) / day);
-      const hours = Math.floor((distance % day) / hour);
-      const minutes = Math.floor(((distance % day) % hour) / minute);
-      const seconds = Math.floor((((distance % day) % hour) % minute) / second);
-
-      setDaysLeft(days);
-      setHoursLeft(hours);
-      setMinutes(minutes);
-      setSeconds(seconds);
+      setTimeLeft(0, 0, 0, 0);
     }
   }
 
-  // console.log(daysLeft);
-  // console.log(hoursLeft);
-  // console.log(minutesLeft);
-  // console.log(secondsLeft);
+  useEffect(() => {
+    if (showCountdown) {
+      function count() {
+        const now = new Date().getTime();
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
 
-  // useEffect(() => {
-  //   if (showCountdown) {
-  //     function count() {
-  //       const today = new Date();
-  //       let remainingDays = dateTerm.split('-')[2] - today.getDate();
-  //       let remainingHours = 24 - today.getHours();
-  //       let remainingMinutes = new Date().getMinutes();
-  //       let remainingSecond = new Date().getSeconds();
-  //       setDaysLeft(remainingDays);
-  //       setHoursLeft(remainingHours);
-  //       setMinutes(remainingMinutes);
-  //       setSeconds(remainingSecond);
-  //     }
-  //     const countDown = setInterval(() => count(), 1000);
-  //     return () => {
-  //       clearInterval(countDown);
-  //     };
-  //   }
-  // }, [showCountdown, dateTerm]);
+        const countDown = new Date(dateTerm).getTime();
+        const distance = countDown - now;
+        const days = Math.floor(distance / day);
+        const hours = Math.floor((distance % day) / hour);
+        const minutes = Math.floor(((distance % day) % hour) / minute);
+        const seconds = Math.floor(
+          (((distance % day) % hour) % minute) / second
+        );
+
+        setTimeLeft(days, hours, minutes, seconds);
+      }
+      const countDown = setInterval(() => count(), 1000);
+      return () => {
+        clearInterval(countDown);
+      };
+    }
+  }, [showCountdown, dateTerm]);
 
   function minDate() {
     const today = new Date();
@@ -73,6 +54,13 @@ const Modal = () => {
     else if (dd < 10) return `${yyy}-${mm}-0${dd}`;
     else if (mm < 10) return `${yyy}-0${mm}-${dd}`;
     return `${yyy}-${mm}-${dd}`;
+  }
+
+  function setTimeLeft(day, hour, minute, second) {
+    setDaysLeft(day);
+    setHoursLeft(hour);
+    setMinutes(minute);
+    setSeconds(second);
   }
 
   function renderForm() {
@@ -110,27 +98,44 @@ const Modal = () => {
   }
 
   function renderCountdown() {
+    const now = new Date().getTime();
+    const distance = new Date(dateTerm).getTime() - now;
+
+    if (distance > 1000) {
+      return (
+        <div className="countdown-container">
+          <h2 className="heading-2">{term}</h2>
+          <div className="count-container">
+            <div className="days">
+              <h2>{daysLeft}</h2>
+              <h3>days</h3>
+            </div>
+            <div className="hours">
+              <h2>{hoursLeft}</h2>
+              <h3>hours</h3>
+            </div>
+            <div className="minutes">
+              <h2>{minutesLeft}</h2>
+              <h3>minutes</h3>
+            </div>
+            <div className="seconds">
+              <h2>{secondsLeft}</h2>
+              <h3>seconds</h3>
+            </div>
+          </div>
+          <Button
+            className="btn-modal"
+            size="lg"
+            onClick={() => setShowCountdown(!showCountdown)}
+            block>
+            reset
+          </Button>
+        </div>
+      );
+    }
     return (
       <div className="countdown-container">
-        <h2 className="heading-2">{term}</h2>
-        <div className="count-container">
-          <div className="days">
-            <h2>{daysLeft}</h2>
-            <h3>days</h3>
-          </div>
-          <div className="hours">
-            <h2>{hoursLeft}</h2>
-            <h3>hours</h3>
-          </div>
-          <div className="minutes">
-            <h2>{minutesLeft}</h2>
-            <h3>minutes</h3>
-          </div>
-          <div className="seconds">
-            <h2>{secondsLeft}</h2>
-            <h3>seconds</h3>
-          </div>
-        </div>
+        <h2 className="heading-2">Timeup</h2>
         <Button
           className="btn-modal"
           size="lg"
@@ -140,18 +145,6 @@ const Modal = () => {
         </Button>
       </div>
     );
-    // return (
-    //   <div className="countdown-container">
-    //     <h2 className="heading-2">Timeup</h2>
-    //     <Button
-    //       className="btn-modal"
-    //       size="lg"
-    //       onClick={() => setShowCountdown(!showCountdown)}
-    //       block>
-    //       reset
-    //     </Button>
-    //   </div>
-    // );
   }
 
   return <div>{showCountdown ? renderCountdown() : renderForm()}</div>;
