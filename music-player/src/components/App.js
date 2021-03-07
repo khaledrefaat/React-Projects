1566935c5ddf9291cd2a9dca5b07b88c49e2baf2
khaledrefaat <khaledrefaat08@gmail.com>
2021-03-07 04:03:01 +0200

    fixing previous button bug

--- music-player/src/components/App.js
+++ music-player/src/components/App.js
@@ -5,102 +5,97 @@ import Controls from './Controls';
 import './App.css';
 
 const App = () => {
-    const [isEnded, setIsEnded] = useState(false);
-    const [progressPercent, setProgressPercent] = useState(0);
-    const [currentTime, setCurrentTime] = useState('0:00');
-    const [duration, setDuration] = useState('0:00');
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [isEnded, setIsEnded] = useState(false);
+  const [progressPercent, setProgressPercent] = useState(0);
+  const [currentTime, setCurrentTime] = useState('0:00');
+  const [duration, setDuration] = useState('0:00');
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const audioPlayer = useRef(null);
+  const audioPlayer = useRef(null);
 
-    useEffect(() => {
-        isPlaying ? audioPlayer.current.play() : audioPlayer.current.pause();
-    }, [isPlaying, audioPlayer]);
+  useEffect(() => {
+    isPlaying ? audioPlayer.current.play() : audioPlayer.current.pause();
+  }, [isPlaying, audioPlayer]);
 
-    useEffect(() => {
-        setIsEnded(false);
-        if (isPlaying) {
-            const timerId = setTimeout(() => {
-                audioPlayer.current.play();
-            }, 150);
-            return () => {
-                clearTimeout(timerId);
-            };
-        }
-    }, [currentIndex, audioPlayer, isPlaying]);
+  useEffect(() => {
+    setIsEnded(false);
+    if (isPlaying) {
+      const timerId = setTimeout(() => {
+        audioPlayer.current.play();
+      }, 150);
+      return () => {
+        clearTimeout(timerId);
+      };
+    }
+  }, [currentIndex, audioPlayer, isPlaying]);
 
-    const songs = [
-        {
-            name: 'jacinto-1',
-            displayName: 'Electric Chill Machine',
-            artist: 'Jacinto Design',
-        },
-        {
-            name: 'jacinto-2',
-            displayName: 'Seven Nation Army (Remix)',
-            artist: 'Jacinto Design',
-        },
-        {
-            name: 'jacinto-3',
-            displayName: 'Goodnight, Disco Queen',
-            artist: 'Jacinto Design',
-        },
-        {
-            name: 'metric-1',
-            displayName: 'Front Row (Remix)',
-            artist: 'Metric/Jacinto Design',
-        },
-    ];
+  const songs = [
+    {
+      name: 'jacinto-1',
+      displayName: 'Electric Chill Machine',
+      artist: 'Jacinto Design',
+    },
+    {
+      name: 'jacinto-2',
+      displayName: 'Seven Nation Army (Remix)',
+      artist: 'Jacinto Design',
+    },
+    {
+      name: 'jacinto-3',
+      displayName: 'Goodnight, Disco Queen',
+      artist: 'Jacinto Design',
+    },
+    {
+      name: 'metric-1',
+      displayName: 'Front Row (Remix)',
+      artist: 'Metric/Jacinto Design',
+    },
+  ];
 
-    const onAudioEnd = () => setIsEnded(true);
+  const onAudioEnd = () => setIsEnded(true);
 
-    const onTimeUpdate = e => {
-        if (isPlaying) {
-            const { duration, currentTime } = e.nativeEvent.srcElement;
-            // setState for music values
-            setProgressPercent((currentTime / duration) * 100);
-            setCurrentTime(currentTime);
-            setDuration(duration);
-        }
-    };
+  const onTimeUpdate = e => {
+    if (isPlaying) {
+      const { duration, currentTime } = e.nativeEvent.srcElement;
+      // setState for music values
+      setProgressPercent((currentTime / duration) * 100);
+      setCurrentTime(currentTime);
+      setDuration(duration);
+    }
+  };
 
-    const isAudioPlaying = playing => setIsPlaying(playing);
+  const isAudioPlaying = playing => setIsPlaying(playing);
 
-    const changeCurrentIndex = index => {
-        setCurrentIndex(index);
-    };
+  const changeCurrentIndex = index => setCurrentIndex(index);
 
-    const cuurentSong = songs[currentIndex];
+  const cuurentSong = songs[currentIndex];
 
-    return (
-        <div className="player-container">
-            <Img songsList={songs} currentIndex={currentIndex} />
-            <h2 id="title">{cuurentSong.displayName}</h2>
-            <h3 id="artist">{cuurentSong.artist}</h3>
-            <audio
-                src={`/music/${cuurentSong.name}.mp3`}
-                ref={audioPlayer}
-                onEnded={onAudioEnd}
-                onTimeUpdate={onTimeUpdate}
-            />
-            <Progress
-                progress={progressPercent}
-                duration={duration}
-                currentTime={currentTime}
-                audioPlayer={audioPlayer.current}
-            />
-            <Controls
-                songsList={songs}
-                isEnded={isEnded}
-                isAudioPlaying={isPlaying => isAudioPlaying(isPlaying)}
-                index={index => changeCurrentIndex(index)}
-            />
-        </div>
-    );
+  return (
+    <div className="player-container">
+      <Img songsList={songs} currentIndex={currentIndex} />
+      <h2 id="title">{cuurentSong.displayName}</h2>
+      <h3 id="artist">{cuurentSong.artist}</h3>
+      <audio
+        src={`/music/${cuurentSong.name}.mp3`}
+        ref={audioPlayer}
+        onEnded={onAudioEnd}
+        onTimeUpdate={onTimeUpdate}
+      />
+      <Progress
+        progress={progressPercent}
+        duration={duration}
+        currentTime={currentTime}
+        audioPlayer={audioPlayer.current}
+      />
+      <Controls
+        songsList={songs}
+        isEnded={isEnded}
+        isAudioPlaying={isPlaying => isAudioPlaying(isPlaying)}
+        index={index => changeCurrentIndex(index)}
+      />
+    </div>
+  );
 };
 
 export default App;
-
-// ClientId - dbb219b78d944e34aaaa6c7df1062326
-// ClientSecret - b6da9b978f694486b1965c4e8e00d9be
--- music-player/src/components/Controls.js
+++ music-player/src/components/Controls.js
@@ -2,64 +2,61 @@ import React, { useState, useEffect, useCallback } from 'react';
 import './Controls.css';
 
 const Controls = ({ isAudioPlaying, songsList, index, isEnded }) => {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [songEnded, setSongEnded] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [songEnded, setSongEnded] = useState(false);
 
-    const setIndex = useCallback(
-        index => {
-            if (index > 0 && currentIndex >= songsList.length - 1) {
-                setCurrentIndex(0);
-            } else if (index < 0 && currentIndex === 0) {
-                setCurrentIndex(songsList.length - 1);
-            } else {
-                setCurrentIndex(currentIndex + 1);
-            }
-        },
-        [currentIndex, songsList]
-    );
+  const setIndex = useCallback(
+    index => {
+      if (currentIndex >= songsList.length - 1 && index > 0) {
+        setCurrentIndex(0);
+        return;
+      } else if (index < 0 && currentIndex === 0) {
+        setCurrentIndex(songsList.length - 1);
+      } else {
+        setCurrentIndex(currentIndex + index);
+      }
+    },
+    [currentIndex, songsList]
+  );
 
-    useEffect(() => {
-        isAudioPlaying(isPlaying);
-    }, [isPlaying, isAudioPlaying]);
+  useEffect(() => {
+    isAudioPlaying(isPlaying);
+  }, [isPlaying, isAudioPlaying]);
 
-    useEffect(() => {
-        index(currentIndex);
-    }, [index, currentIndex]);
+  useEffect(() => {
+    index(currentIndex);
+  }, [index, currentIndex]);
 
-    useEffect(() => {
-        setTimeout(() => setSongEnded(isEnded), 500);
-    }, [isEnded]);
+  useEffect(() => {
+    setTimeout(() => setSongEnded(isEnded), 500);
+  }, [isEnded]);
 
-    useEffect(() => {
-        if (songEnded) {
-            setIndex(1);
-            setSongEnded(false);
-        }
-    }, [songEnded, setIndex]);
+  useEffect(() => {
+    if (songEnded) {
+      setIndex(1);
+      setSongEnded(false);
+    }
+  }, [songEnded, setIndex]);
 
-    const playClass = `fas fa-${isPlaying ? 'pause' : 'play'} main-button`;
+  const playClass = `fas fa-${isPlaying ? 'pause' : 'play'} main-button`;
 
-    return (
-        <div className="player-controls">
-            {/* <i class="fas fa-volume-up" /> */}
-            <i
-                className="fas fa-backward"
-                title="previous"
-                onClick={() => setIndex(-1)}
-            />
-            <i
-                className={playClass}
-                title="play"
-                onClick={() => setIsPlaying(!isPlaying)}
-            />
-            <i
-                className="fas fa-forward"
-                title="next"
-                onClick={() => setIndex(1)}
-            />
-        </div>
-    );
+  return (
+    <div className="player-controls">
+      {/* <i class="fas fa-volume-up" /> */}
+      <i
+        className="fas fa-backward"
+        title="previous"
+        onClick={() => setIndex(-1)}
+      />
+      <i
+        className={playClass}
+        title="play"
+        onClick={() => setIsPlaying(!isPlaying)}
+      />
+      <i className="fas fa-forward" title="next" onClick={() => setIndex(1)} />
+    </div>
+  );
 };
 
 export default Controls;
