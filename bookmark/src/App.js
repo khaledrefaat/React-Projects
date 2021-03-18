205935e9cba34fd1189b1b5b7ffed5759da17f00
khaledrefaat <khaledrefaat08@gmail.com>
2021-03-18 23:08:46 +0200

    bookmark initial commit

---
+++ bookmark/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-background">
+      <Container>App</Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ bookmark/src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+import ReactDom from 'react-dom';
+import App from './App';
+
+ReactDom.render(<App />, document.getElementById('root'));
