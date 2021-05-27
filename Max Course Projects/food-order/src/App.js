98962690b880067c905d5148a4bb1ca3b889fdc1
khaledrefaat <khaledrefaat08@gmail.com>
2021-05-27 21:41:06 +0200

    meals files added to food-order

--- Max Course Projects/food-order/src/App.js
+++ Max Course Projects/food-order/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <div>
       <Header />
-      App
+      <Meals />
     </div>
   );
 };
