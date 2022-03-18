6d4b1780e3340b71e7a6b805701f9e8a7d5b2d9a
khaledrefaat <khaledrefaat08@gmail.com>
2022-03-18 02:44:11 +0200

    fixing cannot find module problem

---
+++ ts-todo/src/@types/index.d.ts
@@ -0,0 +1,6 @@
+declare module '../assets/*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
